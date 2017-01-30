005d0ef3af2a73653b59449f29a7b458d6876b7c
dani2918 <dani2918@vandals.uidaho.edu>
2017-01-30 15:10:53 -0800

    Update from init

--- Project1/Project1/main.swift
+++ Project1/Project1/main.swift
@@ -3,10 +3,32 @@
 //  Project1
 //
 //  Created by Matt Daniel on 1/30/17.
-//  Copyright © 2017 Matthew Daniel. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
 
+
+let board = Board()
+let user = User(sendBoard: board)
+
+print("Enter u for user mode; enter q to quit at any time")
+print("Enter Mode: ")
+let mode = readLine()
+print()
+
+
+board.printBoard()
+
+if(mode == "u")
+{
+    while(true)
+    {
+        user.promptUser()
+    }
+}
+
+
+
+//board.checkBoard()
+//board.printBoard()
